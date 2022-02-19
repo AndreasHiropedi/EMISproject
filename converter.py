b283306092b944cfea5b866e3484b90045a1c210
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-02-19 17:05:48 +0000

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,2 +1,12 @@
 import json
 import csv
+
+def convertFile():
+    json_file = open('./data/Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json')
+    data = json.loads(json_file)
+    
+    csv_data = open('./data/Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.csv', 'w')
+    csv_writer = csv.writer(csv_data)
+    
+    
+    csv_data.close()
