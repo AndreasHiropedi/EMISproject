e1573f19c22edc3274ae04d3241725f4c3bbc0b4
Andreas Hiropedi <s2015345@ed.ac.uk>
2022-02-19 19:59:46 +0000

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,19 +2,29 @@ import json
 import csv
 
 def convertFile():
+    # load the file and retrieve its contents
     json_file = open('./data/Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json', 'r')
     data = json.load(json_file)
     keys = list(data.keys())
     values = data.values()
 
+    # identify all the dictionary keys in the file contents
     for val in values:
+        # after looking at all the files, they all seem to have one entry as a list
+        # which has several dictionaries. the other entries are single value strings
         if type(val) == list:
+            # for the list, we will handle a special case using separate functions
             headers = handleTheList(val)
             for header in headers:
                 keys.append(header)
-    
+
+    # writing to a CSV file, which will store the equivalent information of the JSON file
+    # but in a tabular format
     csv_data = open('./data/Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.csv', 'w')
     csv_writer = csv.writer(csv_data)
+    # add all the columns
+    csv_writer.writerow(keys)
+    # and the data
     
     csv_data.close()
     json_file.close()
